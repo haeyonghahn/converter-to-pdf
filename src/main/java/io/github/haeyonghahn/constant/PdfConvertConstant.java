fcbe4679a04b89460b2afca3246739479453108d
haeyonghahn <yong80211@gmail.com>
2024-12-21 02:55:55 +0900

    Test: 테스트 코드 수정

--- src/main/java/io/github/haeyonghahn/constant/PdfConvertConstant.java
+++ src/main/java/io/github/haeyonghahn/constant/PdfConvertConstant.java
@@ -4,7 +4,7 @@ public class PdfConvertConstant {
 
 	public static String getTemporaryDirectory() {
 		if (System.getProperty("temp.dir") == null) {
-			return System.getProperty("user.dir") + "/src/main/resources/static/";
+			return System.getProperty("user.dir") + "/src/main/resources/";
 		} else {
 			return System.getProperty("temp.dir");
 		}
