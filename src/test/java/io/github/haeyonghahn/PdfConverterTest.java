b377526ad43db3535dcea4abc5b77a8ead56ceca
haeyonghahn <yong80211@gmail.com>
2024-12-21 02:53:21 +0900

    Test: 테스트 코드 수정

--- src/test/java/io/github/haeyonghahn/PdfConverterTest.java
+++ src/test/java/io/github/haeyonghahn/PdfConverterTest.java
@@ -2,17 +2,15 @@ package io.github.haeyonghahn;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.FileOutputStream;
+import java.io.File;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import io.github.haeyonghahn.constant.PdfConvertConstant;
 import io.github.haeyonghahn.mock.FakePdfSource;
+import io.github.haeyonghahn.source.ImageSource;
 import io.github.haeyonghahn.source.PdfSource;
 
 @DisplayName("PdfConverter 테스트")
@@ -37,14 +35,13 @@ public class PdfConverterTest {
 	@Test
 	void convert() throws IOException {
 		// given
-		PdfSource pdfSource = new FakePdfSource();
-		OutputStream out = new FileOutputStream(pdfSource.getOutputPath());
-		out.close();
+		File tempFile = File.createTempFile( "test", ".jpg", new File(PdfConvertConstant.getTemporaryDirectory()));
+		PdfSource pdfSource = new ImageSource(tempFile);
 
 		// when
 		PdfConverter pdfConverter = new PdfConverter(pdfSource);
 		Pdf pdf = pdfConverter.convert(pdfSource);
-		Files.deleteIfExists(Path.of(PdfConvertConstant.getTemporaryDirectory() + "test.jpg"));
+		tempFile.delete();
 
 		// then
 		assertEquals(pdf.getBytes(), pdfSource.getOutputBytes());
--- src/test/java/io/github/haeyonghahn/encrypt/PasswordBasedEncryptModuleTest.java
+++ src/test/java/io/github/haeyonghahn/encrypt/PasswordBasedEncryptModuleTest.java
@@ -2,18 +2,12 @@ package io.github.haeyonghahn.encrypt;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import io.github.haeyonghahn.constant.PdfConvertConstant;
 import io.github.haeyonghahn.mock.FakePdfSource;
-import io.github.haeyonghahn.source.PdfSource;
 
 @DisplayName("PasswordBasedEncryptModule 테스트")
 public class PasswordBasedEncryptModuleTest {
@@ -29,24 +23,5 @@ public class PasswordBasedEncryptModuleTest {
 		RuntimeException exception = assertThrows(RuntimeException.class, passwordBasedEncryptModule::execute);
 
 		assertTrue(exception.getMessage().contains("File does not exist."));
-	}
-
-	@DisplayName("pdf 로 변환할 파일이 존재하지만 pdf 로 변환이 안될 경우 예외를 발생시킨다.")
-	@Test
-	void notConvert() throws IOException {
-		// given
-		PdfSource pdfSource = new FakePdfSource();
-		OutputStream out = new FileOutputStream(pdfSource.getOutputPath());
-		out.close();
-
-		PasswordBasedEncryptModule passwordBasedEncryptModule = new PasswordBasedEncryptModule("qwer1234", "qwer1234");
-		passwordBasedEncryptModule.setPdfSource(pdfSource);
-
-		// when
-		RuntimeException exception = assertThrows(RuntimeException.class, passwordBasedEncryptModule::execute);
-		Files.deleteIfExists(Path.of(PdfConvertConstant.getTemporaryDirectory() + "test.jpg"));
-
-		// then
-		assertTrue(exception.getMessage().contains("An error occurred while encrypting the PDF file."));
 	}
 }
